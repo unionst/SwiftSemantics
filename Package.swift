fb830c7ba5fbe913247118b1a6b85503e01e4b16
blsage <ben@sage.me>
2025-01-25 18:18:19 -0500

    Updated package dependency

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftSemantics",
+    platforms: [
+        .macOS(.v10_15),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -14,16 +17,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "SwiftSyntax",
-                 url: "https://github.com/apple/swift-syntax.git",
-                 .exact("0.50300.0")),
+        .package(url: "https://github.com/apple/swift-syntax", .branch("0.50600.0-SNAPSHOT-2022-01-24"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftSemantics",
-            dependencies: ["SwiftSyntax"]
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax")
+            ]
         ),
         .testTarget(
             name: "SwiftSemanticsTests",
